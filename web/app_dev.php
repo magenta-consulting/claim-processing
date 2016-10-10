30f83a69375993d6a79eceedbb8c65088ced139d
mikedu <mikedutuandu@gmail.com>
2016-10-10 15:31:53 +0700

    update code

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,15 +4,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    // return only the headers and not the content
-    // only allow CORS if we're doing a GET - i.e. no saving for now.
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET') {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Headers: X-Requested-With');
-    }
-    exit;
-}
+//if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//    // return only the headers and not the content
+//    // only allow CORS if we're doing a GET - i.e. no saving for now.
+//    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET') {
+//        header('Access-Control-Allow-Origin: *');
+//        header('Access-Control-Allow-Headers: X-Requested-With');
+//    }
+//    exit;
+//}
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
 // for more information
@@ -20,13 +20,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1','55.55.55.5', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
+//if (isset($_SERVER['HTTP_CLIENT_IP'])
+//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1','55.55.55.5', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
+//) {
+//    header('HTTP/1.0 403 Forbidden');
+//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//}
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
